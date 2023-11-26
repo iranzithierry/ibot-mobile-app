05139f73913c6f2af2a6da59f343f94cf06b21cb
Iranzi Dev <iranzithierry12@gmail.com>
2023-11-26 09:15:34 +0200

    Update ChatContent.js

--- components/ChatContent.js
+++ components/ChatContent.js
@@ -1,24 +1,54 @@
-import { View, Text, ScrollView, TouchableOpacity, RefreshControl, StyleSheet } from 'react-native'
-import React, { useContext, useEffect, useRef, useState, useMemo, useCallback } from 'react'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import React, { useContext, useEffect, useMemo, useCallback } from 'react';
+import { View, Text, TouchableOpacity, FlatList, RefreshControl, StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { styled } from 'nativewind';
-import Context from '../context/context'
+import Context from '../context/context';
 import { deleteData, getData } from '../utils/asyncStorage';
 
-const StyledView = styled(View)
+const StyledView = styled(View);
+
+const MessageItem = React.memo(({ item, index, onSelect, onLongPress, visibleTimeIndex, selected }) => {
+    const prevItem = index > 0 ? message[index - 1] : null;
+    const nextItem = index < message.length - 1 ? message[index + 1] : null;
+
+    return (
+        <StyledView className={`shadow ${selected.includes(item) ? 'my-0.5  p-0.5' : ''}`} key={index} style={{ backgroundColor: selected.includes(item) ? 'rgba(4, 120, 87, 0.4)' : 'transparent' }}>
+            <TouchableOpacity
+                className={`${item.sender === 'user' ? 'bg-[#CBD5E1]' : 'bg-emerald-700'} ${nextItem && item.sender === nextItem.sender ? 'mb-0.5' : 'mb-2'} flex flex-row py-2 px-4 ${item.sender === 'user' ? 'self-end' : 'self-start'} w-fit shadow shadow-slate-900`}
+                style={{
+                    borderTopRightRadius: (prevItem && item.sender === prevItem.sender && item.sender === 'user') ? 8 : 20,
+                    borderTopLeftRadius: (prevItem && item.sender === prevItem.sender && item.sender === 'bot') ? 8 : 20,
+                    borderBottomRightRadius: (nextItem && item.sender === nextItem.sender && item.sender === 'user') ? 8 : 20,
+                    borderBottomLeftRadius: (nextItem && item.sender === nextItem.sender && item.sender === 'bot') ? 8 : 20,
+                    maxWidth: wp(75)
+                }}
+                onPress={() => (selectingActive ? onSelect(item) : changeVisibleTimeIndex(index))}
+                onLongPress={() => onLongPress(item)}
+                activeOpacity={0.8}
+            >
+                <Text className={`${item.sender === 'user' ? 'text-slate-900' : 'text-white'} font-sans_regular text-base`}>
+                    {item.content}
+                </Text>
+            </TouchableOpacity>
+            {visibleTimeIndex === index && (
+                <Text className={`'text-slate-900/50 -mt-1 mx-0.5 font-sans_bold ${item.sender === 'user' ? 'self-end' : 'self-start'}`} style={{ fontSize: 10 }}>
+                    {item.time}
+                </Text>
+            )}
+        </StyledView>
+    );
+});
 
 const ChatContent = () => {
-    const scrollViewRef = useRef();
     const { message, setMessage, selectingActive, setSelectingActive, selected, setSelected } = useContext(Context);
-    const [visibleTimeIndex, setVisibleTimeIndex] = useState(null)
-    const [refreshing, setIsRefreshing] = useState(false)
-    const [loading, setLoading] = useState(true);
+    const [visibleTimeIndex, setVisibleTimeIndex] = useState(null);
+    const [refreshing, setIsRefreshing] = useState(false);
 
     const scrollToTheEnd = () => {
         setTimeout(() => {
             scrollViewRef?.current?.scrollToEnd({ animated: true });
-        }, 200)
-    }
+        }, 200);
+    };
 
     const onSelect = useCallback((item) => {
         let index = selected.indexOf(item);
@@ -31,7 +61,7 @@ const ChatContent = () => {
         }
 
         setSelected(newList);
-        setSelectingActive(newList.length > 0)
+        setSelectingActive(newList.length > 0);
     }, [selected, setSelectingActive, setSelected]);
 
     const onLongPress = useCallback((item) => {
@@ -41,85 +71,63 @@ const ChatContent = () => {
 
     const onRefresh = async () => {
         setIsRefreshing(true);
-        const storageMessages = await getData('messages')
+        const storageMessages = await getData('messages');
         if (storageMessages) {
-            const jsonValue = JSON.parse(storageMessages)
+            const jsonValue = JSON.parse(storageMessages);
             if (jsonValue && jsonValue.length > message.length) {
-                setMessage(jsonValue)
+                setMessage(jsonValue);
             }
         }
-        setIsRefreshing(false)
+        setIsRefreshing(false);
     };
 
     const changeVisibleTimeIndex = (index) => {
         if (index === visibleTimeIndex) {
-            setVisibleTimeIndex(null)
+            setVisibleTimeIndex(null);
             return;
         }
-        setVisibleTimeIndex(index)
-    }
+        setVisibleTimeIndex(index);
+    };
 
     useEffect(() => {
-        scrollToTheEnd()
-    }, [message])
+        scrollToTheEnd();
+    }, [message]);
 
     useEffect(() => {
         (async () => {
-            const storageMessages = await getData('messages')
+            const storageMessages = await getData('messages');
             if (storageMessages) {
-                const jsonValue = JSON.parse(storageMessages)
+                const jsonValue = JSON.parse(storageMessages);
                 if (jsonValue && jsonValue.length > message.length) {
-                    setMessage(jsonValue)
+                    setMessage(jsonValue);
                 }
             }
-        })()
-    }, [])
+        })();
+    }, []);
 
     const memoizedMessages = useMemo(() => message, [message]);
 
     return (
-        <ScrollView
-            ref={scrollViewRef}
+        <FlatList
+            data={memoizedMessages}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+                <MessageItem
+                    item={item}
+                    index={index}
+                    onSelect={onSelect}
+                    onLongPress={onLongPress}
+                    visibleTimeIndex={visibleTimeIndex}
+                    selected={selected}
+                />
+            )}
             bounces={true}
             className="flex flex-col mb-0.5 px-1"
             showsVerticalScrollIndicator={true}
             refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} colors={['#005E38', '#34ab7c']} />}
-        >
-            {
-                memoizedMessages.length !== 0 && memoizedMessages.map((item, index) => {
-                    const prevItem = memoizedMessages[index - 1];
-                    const nextItem = memoizedMessages[index + 1];
-                    return (
-                        <StyledView className={`shadow ${selected.includes(item) ? 'my-0.5  p-0.5' : ''}`} key={index} style={{ backgroundColor: selected.includes(item) ? 'rgba(4, 120, 87, 0.4)' : 'transparent' }}>
-                            <TouchableOpacity
-                                className={`${item.sender === 'user' ? 'bg-[#CBD5E1]' : 'bg-emerald-700'} ${nextItem && item.sender === nextItem.sender ? 'mb-0.5' : 'mb-2'} flex flex-row py-2 px-4 ${item.sender === 'user' ? 'self-end' : 'self-start'} w-fit shadow shadow-slate-900`}
-                                style={{
-                                    borderTopRightRadius: (prevItem && item.sender === prevItem.sender && item.sender === 'user') ? 8 : 20,
-                                    borderTopLeftRadius: (prevItem && item.sender === prevItem.sender && item.sender === 'bot') ? 8 : 20,
-                                    borderBottomRightRadius: (nextItem && item.sender === nextItem.sender && item.sender === 'user') ? 8 : 20,
-                                    borderBottomLeftRadius: (nextItem && item.sender === nextItem.sender && item.sender === 'bot') ? 8 : 20,
-                                    maxWidth: wp(75)
-                                }}
-                                onPress={() => (selectingActive ? onSelect(item) : changeVisibleTimeIndex(index))}
-                                onLongPress={() => onLongPress(item)}
-                                activeOpacity={0.8}
-                            >
-                                <Text className={`${item.sender === 'user' ? 'text-slate-900' : 'text-white'} font-sans_regular text-base`}>
-                                    {item.content}
-                                </Text>
-                            </TouchableOpacity>
-                            {visibleTimeIndex === index && (
-                                <Text className={`'text-slate-900/50 -mt-1 mx-0.5 font-sans_bold ${item.sender === 'user' ? 'self-end' : 'self-start'}`} style={{ fontSize: 10 }}>
-                                    {item.time}
-                                </Text>
-                            )}
-                        </StyledView>
-                    )
-                })
-            }
-        </ScrollView>
-    )
-}
+        />
+    );
+};
 
 export default ChatContent;
-                            
+                    
